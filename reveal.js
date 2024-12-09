b73a38c7a8c224943ff61a4202d0187cd8a202dd
Adriano De Oliveira Ramos <142144501+AdrianoRamosadr@users.noreply.github.com>
2024-12-09 11:45:46 +0100

    Initial commit

---
+++ reveal.js
@@ -0,0 +1,33 @@
+let menuMo = document.getElementById('menu-mobile')
+
+menuMo.classList.toggle('ativar')
+
+
+
+
+function mostrarBox(){
+    let mostrar = document.querySelector('.esconder')
+    mostrar.classList.toggle('exibir')
+
+   
+}
+
+
+
+
+// scrollReveal({reset:true})
+
+// //Topo do site
+
+// scrollReveal().reveal('.texto-topo',
+//     {
+//         duration: 2000,
+//         distance: '90px'
+//     }
+// )
+// revelar.reveal('.box',
+//     {
+//         duration: 2000,
+//         distance: '90px'
+//     }
+// )
