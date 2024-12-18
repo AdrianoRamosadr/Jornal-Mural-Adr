d18640f823705d3953bba9520df0f55552296065
Adriano De Oliveira Ramos <142144501+AdrianoRamosadr@users.noreply.github.com>
2024-12-18 07:14:07 +0100

    Atualizei o meu mobile
    Ainda em manutenção

--- reveal.js
+++ reveal.js
@@ -1,16 +1,64 @@
-let menuMo = document.getElementById('menu-mobile')
-
-menuMo.classList.toggle('ativar')
 
 
 
+function mostrarBox(element) {
+    const section = element.closest('.ver-mais');
+    const esconder = section.querySelector('.esconder');
+    const allSections = document.querySelectorAll('.ver-mais');
+    document.querySelectorAll('.ver-mais')
+    const prevScrollY = window.scrollY
 
-function mostrarBox(){
-    let mostrar = document.querySelector('.esconder')
-    mostrar.classList.toggle('exibir')
+    // Fecha todas as outras seções
+    allSections.forEach(sec => {
+        if (sec !== section) {
+            sec.querySelector('.esconder').classList.remove('mostrar');
+            sec.querySelector('.text-verMais').innerHTML = 'Ver mais &dArr;';
+            sec.querySelector('.text-verMais').style.marginTop = '10px';
+        }
+    });
 
-   
+    if (esconder.classList.contains('mostrar')) {
+        esconder.classList.remove('mostrar');
+        element.innerHTML = 'Ver mais &dArr;';
+        element.style.marginTop = '10px';
+    } else {
+        esconder.classList.add('mostrar');
+        element.innerHTML = 'Ver menos &uArr;';
+        element.style.marginTop = '30px';
+    }
+
+    //Manter a posição da tela
+    window.scrollY({top:prevScrollY})
 }
+
+function animar(){
+    const btn = document.getElementById('btn-menu')
+    const menu = document.getElementById('menu-mobile')
+   menu.classList.toggle('abrir')
+    btn.classList.toggle('ativando')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // let mostrar = document.querySelector('.esconder')
+    // mostrar.classList.toggle('exibir')
+
 
 
 
